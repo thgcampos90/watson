f1aa973879bbd676282ee68d85e6a7e80ca73fbe
Thiago <thgcampos90@gmail.com>
2018-05-13 21:09:07 -0300

    First commit

---
+++ Cliente_2/Cliente 2.json
@@ -0,0 +1,1 @@
+Cliente 2
---
+++ readDatabase.py
@@ -0,0 +1,24 @@
+import pandas
+
+#print(pandas.read_excel('settings.xls'))
+
+D = pandas.read_excel('settings.xls')
+
+workspace = None
+password  = None 
+path   = None
+
+#for i in range(0,2):
+#    print(D["workspace"][i])
+#    print(D["password"][i])
+#    print(D["path"][i])
+
+
+for i in range(0,2):
+    workspace = D['workspace'][i]
+    password  = D['password'][i]
+    path = D['path'][i] + '/' + workspace + '.json' 
+    F = open(path,'w')
+    F.write(workspace)
+    F.close()    
+
